7d5d11252d087a564ee6e5244a78448de9f7f3fb
HossamMuhammedOmar <hossamomar9876@gmail.com>
2022-04-04 18:14:45 +0200

    respons tasawq app

---
+++ app.js
@@ -0,0 +1,17 @@
+// let menu = document.querySelector(".phone-menu");
+// let header = document.getElementsByTagName("header")[0];
+// let social = document.querySelector(".social-container");
+// let closeMenu = document.querySelector(".phone-close");
+// menu.addEventListener("click", (e) => {
+//   header.style.display = "flex";
+//   header.style.flexDirection = "column";
+//   menu.style.display = "none";
+//   social.style.display = "none";
+//   closeMenu.classList.add("phone-close-style");
+// });
+
+// closeMenu.addEventListener("click", () => {
+//   closeMenu.classList.remove("phone-close-style");
+//   menu.style.display = "inline-block";
+//   header.style.display = "none";
+// });
